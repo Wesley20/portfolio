f93ff506cfb1856fcb4df4e3c0cc7e4442688874
Wesley20 <rpwesley20@gmail.com>
2024-03-24 22:59:28 -0300

    teste

---
+++ src/app/next.config.js
@@ -0,0 +1,10 @@
+export const whatsapp = {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'icons8.com',
+      port: '',
+      pathname: '/icon/964RahB4l606/whatsapp',
+    },
+  ],
+};
--- src/app/pages/footer/index.jsx
+++ src/app/pages/footer/index.jsx
@@ -1,26 +1,27 @@
 import { Container, Typography } from "@mui/material";
 import styles from "./index.module.sass";
 import Image from "next/image";
-import instagram from "../../../../imagem/footer/instagram-round-svgrepo-com.svg";
-import facebook from "../../../../imagem/footer/facebook-round-svgrepo-com.svg";
-import linkedin from "../../../../imagem/footer/linkedin-round-svgrepo-com.svg";
+import instagram from "../../../../imagem/footer/icons8-instagram.svg";
+import facebook from "../../../../imagem/footer/icons8-facebook.svg";
+import linkedin from "../../../../imagem/footer/icons8-linkedin.svg";
+import whatsapp from "../../../../imagem/footer/icons8-whatsapp.svg";
 import Link from "next/link";
+// import { whatsapp } from "@/app/next.config";
 
 const Footer = () => {
   return (
     <>
       <Container className={styles.footer}>
-        <Typography>TEste</Typography>
+        
         <Container className={styles.contact}>
           <Link
             href="https://www.instagram.com/wesley.costa20/"
             target="_blank"
           >
             <Image
+              className={styles.social}
               src={instagram}
               alt="Instagram image button"
-              width={20}
-              height={20}
             />
           </Link>
           <Link
@@ -28,10 +29,9 @@ const Footer = () => {
             target="_blank"
           >
             <Image
+              className={styles.social}
               src={facebook}
               alt="Facebook image button"
-              width={20}
-              height={20}
             />
           </Link>
           <Link
@@ -39,13 +39,25 @@ const Footer = () => {
             target="_blank"
           >
             <Image
+              className={styles.social}
               src={linkedin}
               alt="Linkedin image button"
-              width={20}
-              height={20}
+            />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/wesley-almeida-da-costa-297411a9/"
+            target="_blank"
+          >
+            <Image
+              className={styles.social}
+              src={whatsapp}
+              alt="Whatsapp image button"
+              width={28}
+              height={28}
             />
           </Link>
         </Container>
+        <Typography sx={{color: "#ddd"}}>icones by: icons8.com </Typography>
       </Container>
     </>
   );
